cdb482e062394ad145ef6a1ec17b42006430f4ea
pablo <pablocastillopecero@gmail.com>
2023-04-06 18:11:13 +0200

    shopping list fragment: card view

--- app/src/main/java/com/example/cookmate/ShoppingListFragment.java
+++ app/src/main/java/com/example/cookmate/ShoppingListFragment.java
@@ -3,21 +3,37 @@ package com.example.cookmate;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ShoppingListFragment extends Fragment {
 
     private FloatingActionButton btnAddShoppingList;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private static final String TAG = "MyActivity";
+    private TextView CartTitle, CartItems;
+
+    private FirebaseAuth mAuth;
 
     public ShoppingListFragment() {
         // Required empty public constructor
@@ -25,6 +41,10 @@ public class ShoppingListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shopping_list, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        String uid = user.getUid();
 
         FloatingActionButton fab = view.findViewById(R.id.btnAddShoppingList);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +55,30 @@ public class ShoppingListFragment extends Fragment {
             }
         });
 
+        Log.w(TAG, "launching shopping cart fragment.");
+
+        CartTitle = view.findViewById(R.id.CartTitle);
+        CartItems = view.findViewById(R.id.CartItems);
+
+
+        db.collection("users").document(uid)
+                .collection("Shopping Lists")
+                .limit(3)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CartTitle.setText(document.getId());
+                                CartItems.setText(document.get("items").toString());
+                                Log.d(TAG, "hf: " + document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
         return view;
     }
 
